1af961aae119dcb1913220e4f83a7f6e52378705
James Prior <jep200404@columbus.rr.com>
2017-11-02 13:31:21 -0400

    largest_factor.py: largest_factor(): Write code that passes the tests.

--- largest_factor.py
+++ largest_factor.py
@@ -1,2 +1,9 @@
 def largest_factor(x):
-    return None
+    divisor = 2
+    while x > 1:
+        quotient, remainder = divmod(x, divisor)
+        if remainder == 0:
+            x = quotient
+        else:
+            divisor += 1
+    return divisor

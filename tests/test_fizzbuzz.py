bed04dcc317956969d6180cd76b931455833e95a
James Prior and Joey <joey@columbus.rr.com>
2017-12-12 00:56:49 -0500

    tests/test_fizzbuzz.py added test of 6

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -8,6 +8,7 @@ number_to_expected_string = {
     2: '2',
     3: 'fizz',
     5: 'buzz',
+    6: 'fizz',
 }
 @pytest.mark.parametrize('number, expected_string', number_to_expected_string.items())
 def test_known_number_returns_expected(number, expected_string):

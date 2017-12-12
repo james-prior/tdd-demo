cf85bba504c4c00a401f747dc2bbac5db3903f27
James Prior and Joey <joey@columbus.rr.com>
2017-12-12 01:03:43 -0500

    tests/test_fizzbuzz.py: added test of 15

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -10,6 +10,7 @@ number_to_expected_string = {
     5: 'buzz',
     6: 'fizz',
     10: 'buzz',
+    15: 'fizzbuzz',
 }
 @pytest.mark.parametrize('number, expected_string', number_to_expected_string.items())
 def test_known_number_returns_expected(number, expected_string):

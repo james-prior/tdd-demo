a240d3a4b935df2cce9ff4ceac6fb2c8e3658a6d
James Prior and Joey <joey@columbus.rr.com>
2017-12-12 00:45:56 -0500

    tests/test_fizzbuzz.py: added buzz test which failes

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -7,6 +7,7 @@ number_to_expected_string = {
     1: '1',
     2: '2',
     3: 'fizz',
+    5: 'buzz',
 }
 @pytest.mark.parametrize('number, expected_string', number_to_expected_string.items())
 def test_known_number_returns_expected(number, expected_string):

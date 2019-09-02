ddeb5c421b3ae75d8cc76e0d71477810f998f3f5
James Prior <jep200404@columbus.rr.com>
2019-09-02 14:09:14 -0400

    Refactor: Extract is_multiple_of_five().
    Co-authored-by: Jon Bennett <jon@jondbennett.com>
    Co-authored-by: James Prior <jep200404@columbus.rr.com>

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,13 @@
 def is_multiple_of_three(x):
     return x % 3 == 0
 
+def is_multiple_of_five(x):
+    return x % 5 == 0
+
 def fizzbuzz(x):
     if is_multiple_of_three(x):
         return 'fizz'
-    if x % 5 == 0:
+    if is_multiple_of_five(x):
         return 'buzz'
     return x
 

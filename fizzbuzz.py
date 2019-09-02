9b09c553c7bf196927e05fa65b5ec0ac94d47e02
James Prior <jep200404@columbus.rr.com>
2019-09-02 13:40:01 -0400

    5 returns 'buzz'
    Co-authored-by: Jon Bennett <jon@jondbennett.com>
    Co-authored-by: James Prior <jep200404@columbus.rr.com>

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(x):
     if x == 3:
         return 'fizz'
+    if x == 5:
+        return 'buzz'
     return x
 
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -7,6 +7,7 @@ number_to_expected_output = {
     2: 2,
     3: 'fizz',
     4: 4,
+    5: 'buzz',
 }
 @pytest.mark.parametrize('number, expected_output', number_to_expected_output.items())
 def test_known_number_returns_expected(number, expected_output):

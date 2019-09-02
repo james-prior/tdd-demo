fc570b0f9b9d389966a0eedb8c54863ab42b2e31
James Prior <jep200404@columbus.rr.com>
2019-09-02 13:16:27 -0400

    1 returns 1
    Co-authored-by: Jon Bennett <jon@jondbennett.com>
    Co-authored-by: James Prior <jep200404@columbus.rr.com>

---
+++ fizzbuzz.py
@@ -0,0 +1,3 @@
+def fizzbuzz(x):
+    return 1
+
---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,10 @@
+import pytest
+
+from fizzbuzz import fizzbuzz
+
+number_to_expected_output = {
+    1: 1,
+}
+@pytest.mark.parametrize('number, expected_output', number_to_expected_output.items())
+def test_known_number_returns_expected(number, expected_output):
+    assert expected_output == fizzbuzz(number)

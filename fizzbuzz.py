9a61665c808eab26429410990a5369b26439e03a
James Prior <jep200404@columbus.rr.com>
2019-09-02 14:31:49 -0400

    Refactor: Add blank line between if statements.
    Co-authored-by: Jon Bennett <jon@jondbennett.com>
    Co-authored-by: James Prior <jep200404@columbus.rr.com>

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,13 @@
 def fizzbuzz(x):
     if is_multiple_of_three(x) and is_multiple_of_five(x):
         return 'fizzbuzz'
+
     if is_multiple_of_three(x):
         return 'fizz'
+
     if is_multiple_of_five(x):
         return 'buzz'
+
     return x
 
 def is_multiple_of_three(x):
